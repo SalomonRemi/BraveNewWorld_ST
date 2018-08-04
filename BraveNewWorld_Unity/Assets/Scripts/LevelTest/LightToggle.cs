f69eea08f510265c9e54d222c6d0c6f1d2c99fcc
remi_salomon <remisalomon.pro@gmail.com>
2018-08-04 20:25:48 +0200

    New - Maintenance room whitebox in progress (75%)

---
+++ BraveNewWorld_Unity/Assets/Scripts/LevelTest/LightToggle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightToggle : MonoBehaviour {
+
+    public List<GameObject> lightList;
+    public bool isActivated;
+
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isActivated)
+            {
+                isActivated = false;
+                for (int i = 0; i < lightList.Count; i++)
+                {
+                    lightList[i].gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                isActivated = true;
+                for (int i = 0; i < lightList.Count; i++)
+                {
+                    lightList[i].gameObject.SetActive(true);
+                }
+            }
+        }
+	}
+}
